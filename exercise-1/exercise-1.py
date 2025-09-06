4f48b25b607aa10df8a4c98c0c23cb816e4085ee
jimtete <48908714+jimtete@users.noreply.github.com>
2025-09-06 12:18:28 +0200

    Update exercise-1.py

--- exercise-1/exercise-1.py
+++ exercise-1/exercise-1.py
@@ -21,12 +21,16 @@ class PetriNet():
         return self.M[place-1]
 
     def is_enabled(self, transition):
-        acc = -1
+        m_idx = []
         for edge in self.f:
-            if (edge[1] == transition):
-                acc = edge[0]
+            if edge[1] == transition:
+                m_idx.append(edge[0] - 1)
 
-        return (self.M[acc-1] == 1)
+        if not m_idx:
+            return False
+
+        return all(self.M[idx] >= 1 for idx in m_idx)
+
         
 
 
@@ -34,40 +38,89 @@ class PetriNet():
         self.M[place-1] += 1
 
     def fire_transition(self, transition):
-        p_s = -1
-        p_t = -1
+        # Gather all input places (place -> transition) and output places (transition -> place).
+        in_places = [edge[0]-1 for edge in self.f if edge[1] == transition]
+        out_places = [edge[1]-1 for edge in self.f if edge[0] == transition]
 
-        for edge in self.f:
-            if (edge[0] == transition):
-                p_t = edge[1]
-            if (edge[1] == transition):
-                p_s = edge[0]
-        
-        self.M[p_s-1] -= 1
-        self.M[p_t-1] += 1
+        # No arcs for this transition -> nothing to do.
+        if not in_places and not out_places:
+            return
 
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+        # Safety: indices must exist.
+        n = len(self.M)
+        if any(i < 0 or i >= n for i in in_places + out_places):
+            raise IndexError("Edge references a place index that doesn't exist.")
 
-p.add_marking(1)  # add one token to place id 1
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+        # Classic Petri net semantics require ALL input places to have >= 1 token.
+        # (If you truly want your new 'any input has a token' rule, replace 'all' with 'any'.)
+        if not all(self.M[i] >= 1 for i in in_places):
+            return  # not enabled under AND semantics
 
-p.fire_transition(-1)  # fire transition A
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+        # Consume one token from each input place.
+        for i in in_places:
+            self.M[i] -= 1
 
-p.fire_transition(-3)  # fire transition C
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+        # Produce one token in each output place.
+        for j in out_places:
+            self.M[j] += 1
 
-p.fire_transition(-4)  # fire transition D
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
 
-p.add_marking(2)  # add one token to place id 2
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+    # def fire_transition(self, transition):
+    #     p_s = -1
+    #     p_t = -1
 
-p.fire_transition(-2)  # fire transition B
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+    #     for edge in self.f:
+    #         if (edge[0] == transition):
+    #             p_t = edge[1]
+    #         if (edge[1] == transition):
+    #             p_s = edge[0]
 
-p.fire_transition(-4)  # fire transition D
-print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+    #     if (self.M[p_s-1]<=0):
+    #         return
 
-# by the end of the execution there should be 2 tokens on the final place
-print(p.get_tokens(4))
+    #     self.M[p_s-1] -= 1
+    #     self.M[p_t-1] += 1
+
+# p = PetriNet()
+
+# p.add_place(1)  # add place with id 1
+# p.add_place(2)
+# p.add_place(3)
+# p.add_place(4)
+# p.add_transition("A", -1)  # add transition "A" with id -1
+# p.add_transition("B", -2)
+# p.add_transition("C", -3)
+# p.add_transition("D", -4)
+
+# p.add_edge(1, -1)
+# p.add_edge(-1, 2)
+# p.add_edge(2, -2).add_edge(-2, 3)
+# p.add_edge(2, -3).add_edge(-3, 3)
+# p.add_edge(3, -4)
+# p.add_edge(-4, 4)
+
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.add_marking(1)  # add one token to place id 1
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.fire_transition(-1)  # fire transition A
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.fire_transition(-3)  # fire transition C
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.fire_transition(-4)  # fire transition D
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.add_marking(2)  # add one token to place id 2
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.fire_transition(-2)  # fire transition B
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# p.fire_transition(-4)  # fire transition D
+# print(p.is_enabled(-1), p.is_enabled(-2), p.is_enabled(-3), p.is_enabled(-4))
+
+# # by the end of the execution there should be 2 tokens on the final place
+# print(p.get_tokens(4))
